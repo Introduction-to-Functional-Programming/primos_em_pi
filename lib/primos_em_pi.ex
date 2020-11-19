00307d4267aecf4555df8da9aec7ac8e37cfba17
Adolfo Neto <adolfo.usp@gmail.com>
2020-11-19 13:36:21 -0300

    first commit

---
+++ lib/primos_em_pi.ex
@@ -0,0 +1,18 @@
+defmodule PrimosEmPi do
+  @moduledoc """
+  Documentation for `PrimosEmPi`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PrimosEmPi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/primos_em_pi_test.exs
@@ -0,0 +1,8 @@
+defmodule PrimosEmPiTest do
+  use ExUnit.Case
+  doctest PrimosEmPi
+
+  test "greets the world" do
+    assert PrimosEmPi.hello() == :world
+  end
+end
