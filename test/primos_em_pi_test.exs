b746f6089dab042abaf7cb98a906cd01c63f6856
Gustavo Brunholi Chierici <gustavobchierici@gmail.com>
2020-11-19 15:00:51 -0300

    is_prime function implemented

--- test/primos_em_pi_test.exs
+++ test/primos_em_pi_test.exs
@@ -4,4 +4,31 @@ defmodule PrimosEmPiTest do
   test "initial sequence" do
     assert PrimosEmPi.sequence(3.14159265358979323846) == 4159265358979323
   end
+
+  test "clean" do
+    assert PrimosEmPi.clean(3.14159265358979323846) == [
+             1,
+             4,
+             1,
+             5,
+             9,
+             2,
+             6,
+             5,
+             3,
+             5,
+             8,
+             9,
+             7,
+             9,
+             3
+           ]
+  end
+
+  test "is_prime?" do
+    assert(PrimosEmPi.is_prime?(2))
+    assert(PrimosEmPi.is_prime?(3))
+    refute(PrimosEmPi.is_prime?(4))
+    assert(PrimosEmPi.is_prime?(5))
+  end
 end
