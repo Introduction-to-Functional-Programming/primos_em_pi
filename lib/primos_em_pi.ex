0b5df949770290bbe1c30a109a5a1dbfdc2110bb
Adolfo Neto <adolfo.usp@gmail.com>
2020-11-25 13:13:16 -0300

    Incorporação código Cristine sobre Primos em sequência.

--- lib/primos_em_pi.ex
+++ lib/primos_em_pi.ex
@@ -3,7 +3,7 @@ defmodule PrimosEmPi do
     num
     |> clean
 
-    4159265358979323
+    4_159_265_358_979_323
   end
 
   def clean(num) do
@@ -14,9 +14,71 @@ defmodule PrimosEmPi do
     |> Enum.map(&String.to_integer(&1))
   end
 
+  # def is_prime?(n) when n in [2, 3], do: true
+
+  # def is_prime?(x) do
+  #   2..floor(:math.sqrt(x)) |> Enum.all?(fn y -> rem(x, y) != 0 end)
+  # end
+
+  # Código Cristine
+
+  @max_digits_constant 4
+
+  def biggest_prime(numbers) do
+    numbers
+    |> Enum.join()
+    |> do_biggest_prime(max_digits(numbers))
+  end
+
+  def do_biggest_prime(numbers_as_string, max_digits) do
+    numbers_as_string
+    |> slices(max_digits)
+    # |> Enum.sort(fn x, y -> String.to_integer(x) > String.to_integer(y) end)
+    |> check(numbers_as_string, max_digits)
+  end
+
+  def check(slices, numbers, max_digits) do
+    slices
+    |> Enum.filter(&is_prime?/1)
+    |> primes(numbers, max_digits)
+  end
+
+  def primes([], _numbers, 1) do
+    :error
+  end
+
+  def primes([], numbers, acc) do
+    numbers
+    |> do_biggest_prime(acc - 1)
+  end
+
+  def primes(primes, _numbers, _acc) do
+    primes
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.max()
+  end
+
+  def is_prime?(n) when is_binary(n), do: is_prime?(String.to_integer(n))
   def is_prime?(n) when n in [2, 3], do: true
 
-  def is_prime?(x) do
-    2..floor(:math.sqrt(x)) |> Enum.all?(fn y -> rem(x, y) != 0 end)
+  def is_prime?(n) when n <= 9973 do
+    Enum.to_list(2..floor(:math.sqrt(n)))
+    |> calc(n)
   end
+
+  def is_prime?(_n), do: false
+
+  def calc([h | _t], n) when rem(n, h) == 0, do: false
+  def calc([_h | t], n), do: calc(t, n)
+  def calc([], _n), do: true
+
+  def slices(_, size) when size <= 0, do: []
+
+  def slices(s, size) do
+    max = String.length(s)
+    for iterate <- 0..max, iterate + size <= max, do: String.slice(s, iterate, size)
+  end
+
+  def max_digits(numbers) when length(numbers) < @max_digits_constant, do: length(numbers)
+  def max_digits(_), do: @max_digits_constant
 end
